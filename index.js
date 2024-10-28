080a544b5f5de1caad581256c6264bbefb249208
Isabirye1515 <isabiryeelijah15@gmail.com>
2024-10-28 05:29:28 +0300

    first changes

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const cors = require('cors');
 const app = express();
 const port = 3003;
 
-// Enable CORS
 app.use(cors({
-  origin: 'https://eamon-net-bwfd.vercel.app', // Frontend URL
+  origin: ['http://localhost:3000', 'https://livi-fragrances.vercel.app', 'https://eamon-net-bwfd.vercel.app'],
   methods: ['POST'],
   allowedHeaders: ['Content-Type']
 }));
+
 
 app.use(bodyParser.json());
 
@@ -43,6 +43,26 @@ app.post('/send-email', (req, res) => {
     res.status(200).send({ message: 'Email sent successfully!' });
   });
 });
+
+app.post('/get-purfumes', (req, res) => {
+  const { name, phone, email,gender,comment } = req.body;
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: 'isabiryeelijah15@gmail.com',
+    subject: `New Order from ${name}`,
+    text: `Name: ${name}|nPhone Number:${phone}\nEmail: ${email}\nGender: ${gender}\nComment: ${comment}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      return res.status(500).send({ message: 'Error sending email', error: error.toString() });
+    }
+    res.status(200).send({ message: 'Email sent successfully!' });
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
